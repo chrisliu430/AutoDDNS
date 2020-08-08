738c7eb9d099976bb614f9247ff7d37c9aab3908
chrisliu430 <hearnechris430@gmail.com>
2020-08-08 21:03:43 +0800

    Update Code

--- main.go
+++ main.go
@@ -21,7 +21,15 @@ func UpdateDDNS() {
 		log.Fatal(err)
 	}
 	context := string(body)
-	fmt.Println(context)
+	status, _ := regexp.Compile("[a-z0-9A-Z.]{1,16}")
+	analysisCode := status.FindAllString(context, -1)
+	if analysisCode[0] == "good" {
+		data := []byte(analysisCode[1])
+		err := ioutil.WriteFile("./IP.txt", data, 0644)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
 }
 
 func main() {
