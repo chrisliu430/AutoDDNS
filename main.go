b54fd2554d314cc43a8cdb8ae6b00d54362e9d05
chrisliu430 <hearnechris430@gmail.com>
2021-06-04 13:14:05 +0800

    Rewrite DDNS methods

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"regexp"
-	//"fmt"
+	"os/exec"
 )
 
 type dns struct {
@@ -18,25 +16,6 @@ type urlSet struct {
 	URL      string `json:"URL"`
 	UserName string `json:"UserName"`
 	UserPwd  string `json:"UserPwd"`
-}
-
-// ParseHTML is parse html format
-func ParseHTML(url string) string {
-	resp, err := http.Get(url)
-	CheckError(err)
-	body, err := ioutil.ReadAll(resp.Body)
-	CheckError(err)
-	resp.Body.Close()
-	return string(body)
-}
-
-// UpdateLog is auto update status to log
-func UpdateLog(wrStatus string) {
-	file, err := os.OpenFile("DDNS.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	CheckError(err)
-	defer file.Close()
-	log.SetOutput(file)
-	log.Println(wrStatus)
 }
 
 func parseJSON() []string {
@@ -53,52 +32,22 @@ func parseJSON() []string {
 	return urlSlice
 }
 
-func updateStatus(status string, data []byte) {
-	UpdateLog(status)
-	err := ioutil.WriteFile("./IP.txt", data, 0644)
-	CheckError(err)
-}
-
 // UpdateDDNS is call google ddns service
-func UpdateDDNS(mode int) {
-	for key, url := range parseJSON() {
-		context := ParseHTML(url)
-		status, _ := regexp.Compile("[a-z0-9A-Z.]{1,16}")
-		analysisCode := status.FindAllString(context, -1)
-		if key == 0 {
-			if analysisCode[0] == "good" {
-				updateStatus("Setup DDNS is sucessful", []byte(analysisCode[1]))
-			} else if analysisCode[0] == "nochg" && mode == 1 {
-				updateStatus("Resetup DDNS is sucessful", []byte(analysisCode[1]))
-			}
-		}
+func UpdateDDNS() {
+	logFile, err := os.OpenFile("./Record", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	CheckError(err)
+	defer logFile.Close()
+	log.SetOutput((logFile))
+	for _, url := range parseJSON() {
+		cmd := exec.Command("curl", url)
+		out, err := cmd.Output()
+		CheckError(err)
+		log.Println(string(out))
 	}
 }
 
 func main() {
-	context := ParseHTML("https://www.myip.com/")
-	ipRules, _ := regexp.Compile("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")
-	ipArr := ipRules.FindAllString(context, 1)
-	nowIP := ipArr[0]
-	file, _ := os.Open("./IP.txt")
-	fileBody, err := ioutil.ReadAll(file)
-	if err != nil {
-		UpdateDDNS(1)
-	}
-	logFile, err := os.OpenFile("DDNS.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	CheckError(err)
-	logInfo, err := logFile.Stat()
-	CheckError(err)
-	if logInfo.Size() > 5000 {
-		os.Remove("DDNS.log")
-	}
-	CheckError(err)
-	storedIP := string(fileBody)
-	if nowIP != storedIP {
-		UpdateDDNS(0)
-	} else {
-		UpdateLog("IP isn't changed")
-	}
+	UpdateDDNS()
 }
 
 // CheckError is check error whatever happen
